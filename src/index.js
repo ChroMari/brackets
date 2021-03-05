4938415a02b8b3914b98afb4d0b5d1a6a0990f04
ChroMari <chromarija@gmail.com>
2021-03-05 13:17:15 +0300

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isCheck = true;
+
+  do {
+    isCheck = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let one= bracketsConfig[i].join('');
+        if (str.indexOf(one)  !== -1) {
+            str = str.slice(0, str.indexOf(one)) + str.slice(str.indexOf(one) + 2); //вырезаем найденную пару скобок
+            isCheck = true;
+        } 
+    }
+  } while (isCheck);
+
+  return str.length === 0;
 }
